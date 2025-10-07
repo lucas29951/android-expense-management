2ee7a679f55daccec3205125cbfa1290e54b2856
max0rx <maxi37556@gmail.com>
2025-10-07 18:44:57 -0300

    se agrega vista principal con sus elementos basicos (top app bar & navigation bar)

---
+++ app/src/main/java/com/labdevs/controldegastos/InformeFragment.java
@@ -0,0 +1,25 @@
+package com.labdevs.controldegastos;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.core.view.OnApplyWindowInsetsListener;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+public class InformeFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_informe, container, false);
+    }
+
+}
--- app/src/main/java/com/labdevs/controldegastos/MainActivity.java
+++ app/src/main/java/com/labdevs/controldegastos/MainActivity.java
@@ -6,19 +6,58 @@ import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+
+import com.labdevs.controldegastos.databinding.ActivityMainBinding;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
+    private List<Fragment> fragments = new ArrayList<>();
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(binding.getRoot());
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        beginFragmentTransaction();
+    }
+
+    private void beginFragmentTransaction() {
+        loadFragments();
+        binding.bottomNav.setOnItemSelectedListener(item -> {
+            if (item.getItemId() == R.id.item_4) {
+                setCurrentFragment(fragments.get(3));
+            }
+            return true;
+        });
+    }
+
+    private void setCurrentFragment(Fragment fragment) {
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_layout,fragment).commit();
+    }
+
+    private void loadFragments() {
+        fragments.add(new BlankFragment());
+        fragments.add(new BlankFragment());
+        fragments.add(new BlankFragment());
+        fragments.add(new InformeFragment());
     }
 }

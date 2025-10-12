6ab84fcf15162debcc0990cd9b3b6019f001c0ad
max0rx <maxi37556@gmail.com>
2025-10-12 17:30:26 -0300

    se agrega botones para cambiar el periodo

--- app/src/main/java/com/labdevs/controldegastos/InformeFragment.java
+++ app/src/main/java/com/labdevs/controldegastos/InformeFragment.java
@@ -12,7 +12,23 @@ import androidx.fragment.app.Fragment;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
+import com.google.android.material.button.MaterialButton;
+
+import java.text.BreakIterator;
+import java.time.LocalDate;
+
 public class InformeFragment extends Fragment {
+
+    private LocalDate fecha;
+    private FiltroGeneralFragment filtroGen;
+    private Fragment filtroPeriodo;
+
+    public InformeFragment() {
+        super();
+        fecha = LocalDate.now();
+        filtroGen = new FiltroGeneralFragment(fecha, FiltroGeneralFragment.TipoFiltroGen.MENSUAL);
+        filtroPeriodo = new FiltroPeriodoFragment(fecha);
+    }
 
     @Nullable
     @Override
@@ -20,30 +36,38 @@ public class InformeFragment extends Fragment {
                              @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_informe, container, false);
+
         Button trailingButton = view.findViewById(R.id.trailingButton);
         Button mainButton = view.findViewById(R.id.mainButton);
-        trailingButton.setOnClickListener(v->showMenu(v,R.menu.menu_boton_informe));
-        mainButton.setOnClickListener(v->showMenu(v,R.menu.menu_boton_informe));
+        trailingButton.setOnClickListener(v->showMenu(view,v,R.menu.menu_boton_informe));
+        mainButton.setOnClickListener(v->showMenu(view,v,R.menu.menu_boton_informe));
+
+        loadFiltroGenFragment();
+
         return view;
     }
 
-    private void showMenu(View v, int menuRes) {
-        PopupMenu popup = new PopupMenu(getActivity(),v);
+    private void showMenu(View v,View button, int menuRes) {
+        PopupMenu popup = new PopupMenu(getActivity(),button);
         popup.getMenuInflater().inflate(menuRes,popup.getMenu());
 
         popup.setOnMenuItemClickListener(item -> {
             int itemId = item.getItemId();
             if (itemId == R.id.menu_informe_opcion_1) {
-                Toast.makeText(getContext(), "Opcion 1", Toast.LENGTH_SHORT).show();
+                changeMainButtonText(v,"Semanal");
+                loadFiltroGenFragment(FiltroGeneralFragment.TipoFiltroGen.SEMANAL);
                 return true;
             } else if (itemId == R.id.menu_informe_opcion_2) {
-                Toast.makeText(getContext(), "Opcion 2", Toast.LENGTH_SHORT).show();
+                changeMainButtonText(v,"Mensual");
+                loadFiltroGenFragment(FiltroGeneralFragment.TipoFiltroGen.MENSUAL);
                 return true;
             } else if (itemId == R.id.menu_informe_opcion_3) {
-                Toast.makeText(getContext(), "Opcion 3", Toast.LENGTH_SHORT).show();
+                changeMainButtonText(v,"Anual");
+                loadFiltroGenFragment(FiltroGeneralFragment.TipoFiltroGen.ANUAL);
                 return true;
             } else if (itemId == R.id.menu_informe_opcion_4) {
-                Toast.makeText(getContext(), "Opcion 4", Toast.LENGTH_SHORT).show();
+                changeMainButtonText(v,"Periodo");
+                getChildFragmentManager().beginTransaction().replace(R.id.filtrosFragment,filtroPeriodo).commit();
                 return true;
             }
             return false;
@@ -52,6 +76,20 @@ public class InformeFragment extends Fragment {
         popup.show();
 
 
+    }
+
+    private void changeMainButtonText(View v, String str) {
+        Button button =  v.findViewById(R.id.mainButton);
+        button.setText(str);
+    }
+
+    private void loadFiltroGenFragment(FiltroGeneralFragment.TipoFiltroGen tipoFiltroGen) {
+        filtroGen = new FiltroGeneralFragment(fecha,tipoFiltroGen);
+        loadFiltroGenFragment();
+    }
+
+    private void loadFiltroGenFragment() {
+        getChildFragmentManager().beginTransaction().replace(R.id.filtrosFragment,filtroGen).commit();
     }
 
 }

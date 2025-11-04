36238d5b6f44a5deb30959250a8689d6a3c62b2e
max0rx <maxi37556@gmail.com>
2025-11-04 01:27:27 -0300

    se implementa menu al campo para seleccionar el tipo de cuenta

--- app/src/main/java/com/labdevs/controldegastos/ManageAccountFragment.java
+++ app/src/main/java/com/labdevs/controldegastos/ManageAccountFragment.java
@@ -7,6 +7,7 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.PopupMenu;
 
 import com.labdevs.controldegastos.databinding.FragmentManageAccountBinding;
 
@@ -27,6 +28,37 @@ public class ManageAccountFragment extends Fragment {
         viewModel.setAppBarTitle(getString(R.string.manege_title_1));
         viewModel.setAppBarNavIcon(true);
 
+        binding.selectType.setOnClickListener(imageView -> showMenu(imageView));
+
         return binding.getRoot();
+    }
+
+    private void showMenu(View button) {
+        PopupMenu popup = new PopupMenu(getActivity(),button);
+        popup.getMenuInflater().inflate(R.menu.menu_boton_tipo_cuenta,popup.getMenu());
+
+        popup.setOnMenuItemClickListener(item -> {
+            int itemId = item.getItemId();
+            if(itemId == R.id.menu_tipo_cuenta_op_1){
+                changeTypeLabelText(getString(R.string.menu_tipo_cuenta_op_texto_1));
+                return true;
+            } else if (itemId == R.id.menu_tipo_cuenta_op_2){
+                changeTypeLabelText(getString(R.string.menu_tipo_cuenta_op_texto_2));
+                return true;
+            } else if(itemId == R.id.menu_tipo_cuenta_op_3){
+                changeTypeLabelText(getString(R.string.menu_tipo_cuenta_op_texto_3));
+                return true;
+            } else if (itemId == R.id.menu_tipo_cuenta_op_4){
+                changeTypeLabelText(getString(R.string.menu_tipo_cuenta_op_texto_4));
+                return true;
+            }
+            return false;
+        });
+
+        popup.show();
+    }
+
+    private void changeTypeLabelText(String text) {
+        binding.tvSelectType.setText(text);
     }
 }
